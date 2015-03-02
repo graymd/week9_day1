4993caad98ad79a3f3a0e066099a9238c8af30a6
Malachi Gray <graymd.cfi@gmail.com>
2015-03-02 15:36:03 -0500

    update readme. clean up js controller and index view

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # week9_day1
+
+March 2nd 2015 - Angular Day 1
+
+Create me a calculator in Angular.
+
+Addition
+Subtraction
+Clear button
+
+-------------------------------------------------------
+requires ruby 2.2.0
+run bundle install
+run rake db:create db:migrate
+run rails server
+point to browser - usually localhost:3000
--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -9,78 +9,52 @@ var indexCtrl = calculator.controller('indexCtrl', function($scope){
 
   $scope.userNumber1 = function() {
     $scope.calcInput = $scope.calcInput.concat(1);
-    console.log(1);
   };
   $scope.userNumber2 = function() {
     $scope.calcInput = $scope.calcInput.concat(2);
-    console.log(2);
   };
   $scope.userNumber3 = function() {
     $scope.calcInput = $scope.calcInput.concat(3);
-    console.log(3);
   };
   $scope.userNumber4 = function() {
     $scope.calcInput = $scope.calcInput.concat(4);
-    console.log(4);
   };
   $scope.userNumber5 = function() {
     $scope.calcInput = $scope.calcInput.concat(5);
-    console.log(5);
   };
   $scope.userNumber6 = function() {
     $scope.calcInput = $scope.calcInput.concat(6);
-    console.log(6);
   };
   $scope.userNumber7 = function() {
     $scope.calcInput = $scope.calcInput.concat(7);
-    console.log(7);
   };
   $scope.userNumber8 = function() {
     $scope.calcInput = $scope.calcInput.concat(8);
-    console.log(8);
   };
   $scope.userNumber9 = function() {
     $scope.calcInput = $scope.calcInput.concat(9);
-    console.log(9);
   };
   $scope.userNumber0 = function() {
     $scope.calcInput = $scope.calcInput.concat(0);
-    console.log(0);
   };
   $scope.userNumberPlus = function() {
     $scope.userInputs.push($scope.calcInput);
     console.log($scope.userInputs);
     $scope.calcInput = '';
     console.log($scope.calcInput);
-    console.log('+');
   };
   $scope.userNumberMinus = function() {
     $scope.userInputs.push($scope.calcInput);
     $scope.calcInput = '-';
     console.log($scope.userInputs);
-    console.log('-');
   };
   $scope.userNumberEquals = function() {
     // add final item to array and then reduce
     $scope.userInputs.push($scope.calcInput);
     $scope.userInputs = $scope.userInputs.map(Number);
-    console.log($scope.userInputs);
     var total = _.reduce($scope.userInputs, function(memo, num){return memo + num; },0);
-    console.log(total);
     $scope.calcInput = total;
     $scope.userInputs = [];
-    console.log('=');
   };
 
 });
-
-
-// var array1 = ['1','2','3'];
-// console.log(array1);
-// var array2 = array1.map(Number);
-// console.log(array2);
-// var total = _.reduce(array2, function(memo, num){return memo + num; }, 0)
-// console.log(total)
-
-
-
